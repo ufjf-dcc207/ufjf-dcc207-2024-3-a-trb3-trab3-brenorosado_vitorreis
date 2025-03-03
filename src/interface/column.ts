6527ba030bef17094193503b20b258542082f444
C9BrenoFR <breno.rosado@estudante.ufjf.br>
2025-03-02 23:43:11 -0300

    Remove configuração de tabelas

--- src/interface/column.ts
+++ src/interface/column.ts
@@ -2,11 +2,4 @@ export type Column = {
     id: number
     name: string,
     type: string,
-    configs: ColumnConfig
-}
-
-export type ColumnConfig = {
-    primaryKey: boolean,
-    notNull: boolean,
-    foreignerKey: number|null,
 }
--- src/interface/table.ts
+++ src/interface/table.ts
@@ -4,10 +4,4 @@ export type Table = {
     id: number
     name: string,
     columns: Column[],
-    configs: TableConfig
-}
-
-export type TableConfig = {
-    primaryKey: number,
-    referenceIn: number[]
 }
