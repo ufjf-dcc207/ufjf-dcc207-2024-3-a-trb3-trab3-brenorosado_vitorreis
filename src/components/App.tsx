ffbe8ef31868113a1cf0c3e20dc6705779d681bd
C9BrenoFR <breno.rosado@estudante.ufjf.br>
2025-03-04 18:27:47 -0300

    Função de criar de ligação

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import InsertionArea from "./InsertionArea";
 import "./App.css";
 import { useState } from "react";
 import { Table } from "../interface/table";
+import { Column } from "../interface/column";
 
 function App() {
   const [tables, setTables] = useState<Table[]>([])
@@ -11,10 +12,22 @@ function App() {
     setTables([...tables, table])
   }
 
+  function createRelation(where: Table, from: Table){
+    const newRelationColumn: Column = {
+      id: Date.now(),
+      name: `${from.name}_id`,
+      type: "int"
+    }
+
+    const tablesCopy = [...tables]
+    tablesCopy.filter(table => table.id === where.id)[0].columns = [...where.columns, newRelationColumn]
+    setTables([...tablesCopy])
+  }
+
   return (
     <div className="app-container">
       <ExhibitionArea tables={tables} />
-      <InsertionArea addTable={addTable} />
+      <InsertionArea tables={tables} addTable={addTable} createRelation={createRelation} />
     </div>
   );
 }
