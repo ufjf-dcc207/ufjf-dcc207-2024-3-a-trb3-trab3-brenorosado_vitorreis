facb98340fbb374e384fdb14a4a35a1fd51c6b1a
C9BrenoFR <breno.rosado@estudante.ufjf.br>
2025-03-04 02:58:59 -0300

    Efeito arratavel para a div

--- src/components/ExhibitionArea.tsx
+++ src/components/ExhibitionArea.tsx
@@ -1,7 +1,37 @@
+import { useRef } from "react";
 import "./ExhibitionArea.css";
 
-function ExhibitionArea() {
-  return <div className="exhibition-area"></div>;
-}
+export default function ExhibitionArea() {
+  const tableRef = useRef<HTMLDivElement>(null);
 
-export default ExhibitionArea;
+  const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
+    const target = e.target as HTMLDivElement;
+    const rect = target.getBoundingClientRect();
+    e.dataTransfer.setData("text/plain", `${e.clientX - rect.left},${e.clientY - rect.top}`);
+  };
+
+  const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
+  const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    const offset = e.dataTransfer.getData("text/plain").split(",");
+    const table = tableRef.current;
+    if (table) {
+      table.style.left = `${e.clientX - parseInt(offset[0], 10)}px`;
+      table.style.top = `${e.clientY - parseInt(offset[1], 10)}px`;
+    }
+  };
+
+  return (
+    <div className="exhibition-area" onDragOver={onDragOver} onDrop={onDrop}>
+      <div
+        ref={tableRef}
+        className="table"
+        draggable
+        onDragStart={onDragStart}
+      ></div>
+    </div>
+  );
+}
